b07d8f01dae00f6ff765d3a795ac201f1407586d
David Carlyn <dcarlyn@kent.edu>
2017-10-17 20:20:53 -0400

    Added Prefabs and Scripts

---
+++ Assets/Scripts/GoToLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToLevel : MonoBehaviour
+{
+
+	public void LoadLevel(string level)
+	{
+		Debug.Log(level);
+		SceneManager.LoadScene(level);
+	}
+}
