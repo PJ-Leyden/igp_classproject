c4fe45d10a935086eef07e190c7d2c064ab4a64f
David Carlyn <dcarlyn@kent.edu>
2017-10-22 13:47:42 -0400

    Added a ScoreManager and a unit tested screen

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	//Total Score of the game
+	public static int score;
+	public Text scoreText;
+	
+	//Add to the score
+	public void AddScore(int points)
+	{
+		score += points;
+		UpdateScoreText();
+		//Debug.Log(score);
+	}
+
+	//Subtract from the score
+	public void SubtractScore(int points)
+	{
+		score -= points;
+		UpdateScoreText();
+	}
+
+	void UpdateScoreText()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+}
