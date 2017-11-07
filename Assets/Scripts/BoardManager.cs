2a2fa7398ce8cf85a77a2d284a02545d880c1532
PJ-Leyden <pleyden@kent.edu>
2017-11-06 21:40:55 -0500

    Major Update. Added auto-random board generation

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+	[Serializable]
+	public class Count {
+		public int minimum;
+		public int maximum;
+
+		public Count(int min, int max) {
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+	public int columns = 7;
+	public int rows = 7;
+	public Count forestCount = new Count (5, 7);
+	public Count lakeCount = new Count (1, 3);
+	public Count mountainCount = new Count (1, 3);
+	public GameObject[] grass_tiles;
+	public GameObject[] lake_tiles;
+	public GameObject[] mountain_tiles;
+	public GameObject[] forest_tiles;
+
+	private Transform boardHolder;
+	private List <Vector3> gridPositions = new List<Vector3>();
+
+	void InitializeList() {
+		gridPositions.Clear();
+		for(int x = 0; x < columns; x++){
+			for(int y = 0; y < rows; y++){
+				gridPositions.Add(new Vector3(x, y, 0f));
+			}
+		}
+	}
+
+	void BoardSetup() {
+		boardHolder = new GameObject("Board").transform;
+	}
+
+	Vector3 RandomPosition() {
+		int randomIndex = Random.Range(0, gridPositions.Count);  
+		Vector3 randomPosition = gridPositions[randomIndex];   
+		gridPositions.RemoveAt(randomIndex);
+		return randomPosition;
+    }
+
+    void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum) {
+    	int objectCount = Random.Range(minimum, maximum + 1);
+    	for(int i = 0; i < objectCount; i++){
+    		Vector3 randomPosition = RandomPosition();
+    		GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+    		GameObject instance = Instantiate(tileChoice, randomPosition, Quaternion.identity) as GameObject;
+    		instance.transform.SetParent(boardHolder);
+    	}
+    }
+
+    void FillBoard(GameObject[] tileArray) {
+    	for(int i = 0; i < gridPositions.Count; i++){
+    		Vector3 curPosition = gridPositions[i];
+    		GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+    		GameObject instance = Instantiate(tileChoice, curPosition, Quaternion.identity) as GameObject;
+    		instance.transform.SetParent(boardHolder);
+    	}
+    }
+
+    public void SetupScene(int level) {
+    	BoardSetup();
+    	InitializeList();
+    	LayoutObjectAtRandom(lake_tiles, lakeCount.minimum, lakeCount.maximum);
+    	LayoutObjectAtRandom(forest_tiles, forestCount.minimum, forestCount.maximum);
+    	LayoutObjectAtRandom(mountain_tiles, mountainCount.minimum, mountainCount.maximum);
+    	FillBoard(grass_tiles);
+    }
+}
---
+++ Assets/Scripts/GameManager_test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_test : MonoBehaviour {
+
+	private BoardManager boardScript;
+
+
+	private int level = 1;
+
+	// Use this for initialization
+	void Awake () {
+		boardScript = GetComponent<BoardManager>();
+		InitGame();
+	}
+
+	void InitGame() {
+		boardScript.SetupScene(level);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
