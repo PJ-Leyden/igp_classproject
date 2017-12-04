4973dfafbe9cf0f86303eea6e0af30e8a3683c01
David Carlyn <dcarlyn@kent.edu>
2017-12-03 23:32:59 -0500

    Added bullet functionality and finished tower script

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public float maxLifeTime = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, maxLifeTime);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		Debug.Log(collision.tag);
+		if(collision.CompareTag("Enemy"))
+		{
+			collision.GetComponent<EnemyHealth>().subtractHealth();
+			Destroy(gameObject);
+		}
+	}
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -6,26 +6,58 @@ public class Tower : MonoBehaviour {
 
 	public Transform center;
 	public Transform cannonEnd;
+	public GameObject bullet;
+	public float bulletSpeed;
+	public float shootTimer;
 
 	private Vector2 centerPos;
 	private Vector2 cannonEndPos;
 	private Quaternion myRotation;
+	private float shootTimeLeft;
 
 	// Use this for initialization
 	void Start () {
 		centerPos = center.position;
 		cannonEndPos = cannonEnd.position;
+		shootTimeLeft = shootTimer;
 
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update() {
+		Rotate();
+		ShootControl();
+	}
+
+	private void Rotate()
+	{
 		//Debug.Log("Center: " + centerPos);
-		Vector2 enemyPos = GameObject.FindGameObjectsWithTag("Enemy")[0].transform.position;
+		int index = GameObject.FindGameObjectsWithTag("Enemy").Length - 1;
+		Vector2 enemyPos = GameObject.FindGameObjectsWithTag("Enemy")[index].transform.position;
 		Vector2 dirToEnemy = enemyPos - centerPos;
 		Vector2 dirOfCannon = cannonEndPos - centerPos;
 
 		myRotation.SetFromToRotation(dirOfCannon, dirToEnemy);
 		transform.rotation = myRotation;
+	}
+
+	private void Shoot()
+	{
+		GameObject myBullet = Instantiate(bullet, cannonEnd.position, Quaternion.identity);
+
+		myBullet.GetComponent<Rigidbody2D>().velocity = transform.up * bulletSpeed;
+	}
+
+	private void ShootControl()
+	{
+		if (shootTimeLeft > 0)
+		{
+			shootTimeLeft -= Time.deltaTime;
+		}
+		else
+		{
+			Shoot();
+			shootTimeLeft = shootTimer;
+		}
 	}
 }
