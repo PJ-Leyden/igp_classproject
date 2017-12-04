7f79ebe707c830272596cd0c3abd03ae2f8dcfda
David Carlyn <dcarlyn@kent.edu>
2017-12-03 22:35:27 -0500

    Created Tower script and fixed enemy movement bug

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+	public Transform center;
+	public Transform cannonEnd;
+
+	private Vector2 centerPos;
+	private Vector2 cannonEndPos;
+	private Quaternion myRotation;
+
+	// Use this for initialization
+	void Start () {
+		centerPos = center.position;
+		cannonEndPos = cannonEnd.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log("Center: " + centerPos);
+		Vector2 enemyPos = GameObject.FindGameObjectsWithTag("Enemy")[0].transform.position;
+		Vector2 dirToEnemy = enemyPos - centerPos;
+		Vector2 dirOfCannon = cannonEndPos - centerPos;
+
+		myRotation.SetFromToRotation(dirOfCannon, dirToEnemy);
+		transform.rotation = myRotation;
+	}
+}
